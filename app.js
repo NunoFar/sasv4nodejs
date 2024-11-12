3f0d210a6feb3d4c1485367edbc72a8c3e32786f
nunof <nfarinha@gmail.com>
2024-11-12 16:39:20 +0000

    path

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(config.baseUrl, indexRouter);
-app.use(path.join(config.baseUrl,'/viewer'), viewerRouter);
+app.use('/', indexRouter);
+app.use('/viewer', viewerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
