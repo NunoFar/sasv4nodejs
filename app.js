4b3311ef01eefb3a634cbef026d44dd7c1c2872a
nunof <nfarinha@gmail.com>
2024-11-12 18:24:50 +0000

    p

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/more', express.static(path.join(__dirname, 'public')));
 
 app.use('/ddc/', indexRouter);
 app.use('/ddc/viewer', viewerRouter);
