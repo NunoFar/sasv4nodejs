f20b338824023bf1cf88eac69ffde9db68b5b86b
nunof <nfarinha@gmail.com>
2024-11-12 17:27:57 +0000

    p

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/ddc/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/ddc', indexRouter);
 app.use('/ddc/viewer', viewerRouter);
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.message = err.message + " Path:" + __dirname;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
