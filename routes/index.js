556836f9edc966fb5a85e96ff71482cf4138a586
nunof <nfarinha@gmail.com>
2024-11-12 10:43:55 +0000

    viewers

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node SAS DDC' });
 });
 
+
+// 
+router.param('id', function (req, res, next, id) {
+  next();
+});
+
+router.get('/viewer/:id', function (req, res) {
+  res.render('viewer', { title: 'The Report Viewer' });
+});
+
 module.exports = router;
