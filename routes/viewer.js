a406a6e5050ec29e6957ae7bda78952ad34949f3
NunoFar <nuno.farinha@timestamp.pt>
2025-03-13 09:25:04 +0000

    Update viewer.js

--- routes/viewer.js
+++ routes/viewer.js
@@ -9,7 +9,7 @@ router.param('id', function (req, res, next, id) {
 router.get('/:id', function(req, res, next) {
   res.render('viewer', {
     title: 'The Report Viewer',
-    sastag: '<sas-report url="https://uqbi11bi.uq.pt" reportUri="/reports/reports/' + req.params.id + '" authenticationType="credentials" class="my-report" hideNavigation></sas-report>'
+    sastag: '<sas-report url="https://uqbi11.uq.pt" reportUri="/reports/reports/' + req.params.id + '" authenticationType="credentials" class="my-report" hideNavigation></sas-report>'
   });
 });
 
