cd3f5e7117efd5506b6ac6b3b135a4b9de9f0b3c
nunof <nfarinha@gmail.com>
2025-03-13 11:19:42 +0000

    router

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'SAS Viya' });
 });
 
+/* GET Report View */
+
+router.param('id', function (req, res, next, id) {
+  next();
+});
+router.get('/viewer/:id', function (req, res, next) {
+  res.render('viewer', {
+    title: 'The Report Viewer',
+    sastag: '<sas-report url="https://uqbi11.uq.pt" reportUri="/reports/reports/' + req.params.id + '" authenticationType="credentials" class="my-report" hideNavigation></sas-report>'
+  });
+});
+
 
 module.exports = router;
